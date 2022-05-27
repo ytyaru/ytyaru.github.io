750764a3a7aedbb24d4f5bf56fa92c641001a463
ytyaru <ytyaru@outlook.jp>
2022-05-27 13:53:45 +0900

    webmensionをmpurseより先に実行するようにした。

--- js/main.js
+++ js/main.js
@@ -1,9 +1,4 @@
 window.addEventListener('DOMContentLoaded', async(event) => {
-    try {
-        window.mpurse.updateEmitter.removeAllListeners()
-          .on('stateChanged', isUnlocked => console.log(isUnlocked))
-          .on('addressChanged', address => console.log(address));
-    } catch(e) { console.debug(e) }
     const mention = new WebMention() 
     await mention.make() 
     tippy('#web-mention-count', {
@@ -15,5 +10,10 @@ window.addEventListener('DOMContentLoaded', async(event) => {
         placement: 'right',
         content: `<a href="https://ytyaru.github.io/">ここのURL</a>を書いて<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="いいね！" data-show-count="false">Tweet</a><script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>すると↓に表示されます。<a href="https://mstdn.jp/">mstdn.jp</a>か<a href="https://pawoo.net/">pawoo</a>でTootしても同じです。`,
     });
+    try {
+        window.mpurse.updateEmitter.removeAllListeners()
+          .on('stateChanged', isUnlocked => console.log(isUnlocked))
+          .on('addressChanged', address => console.log(address));
+    } catch(e) { console.debug(e) }
 });
 
